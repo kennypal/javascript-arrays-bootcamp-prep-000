cf6bf717f715f3ebbcea2b457ea100fd4c662fb3
kennypal <kpalces@yahoo.com>
2018-09-01 03:35:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,14 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  return array.slice(1);
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop();
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
+
