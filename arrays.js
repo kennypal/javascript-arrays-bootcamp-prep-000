3348257b045816d17a5a795f140386a6a9c658a4
kennypal <kpalces@yahoo.com>
2018-09-01 03:20:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,56 +1,17 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"]
 
-function addElementToBeginningOfArray(Array, Element) {
-  var addElementToBeginningOfArray = [Array]
-  addElementToBeginningOfArray.unshift(Element)
-  return array
-
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(Array, Element) {
-  var destructivelyAddElementToBeginningOfArray = [Array]
-  [Element, ...destructivelyAddElementToBeginningOfArray]
-  return new_array
-
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
 }
 
-function addElementToEndOfArray(Array, Element) {
-  var addElementToEndOfArray = [Array]
-  [...addElementToEndOfArray, Element]
-  return array
-
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
 }
 
-function destructivelyAddElementToEndOfArray(Array, Element) {
-  var destructivelyAddElementToEndOfArray = [Array]
-  destructivelyAddElementToEndOfArray.push(Element)
-  return new_array
-
-}
-
-var accessElementInArray = [Red, White, Blue]
-
-function destructivelyRemoveElementFromBeginningOfArray(Array, Element) {
-  var destructivelyRemoveElementFromBeginningOfArray = [Element, Array]
-  destructivelyRemoveElementFromBeginningOfArray.shift()
-  return array.unshift()
-
-}
-
-function removeElementFromBeginningOfArray(Array, Element) {
-  var removeElementFromBeginningOfArray = [Element, Array]
-  return removeElementFromBeginningOfArray.slice(1)
-
-}
-
-function destructivelyRemoveElementFromEndOfArray(Array, Element) {
-  var destructivelyRemoveElementFromEndOfArray = [Array, Element]
-  return destructivelyRemoveElementFromEndOfArray.push()
-
-}
-
-function removeElementFromEndOfArray(Array, Element) {
-  var removeElementFromEndOfArray = [Array, Element]
-  return removeElementFromEndOfArray.slice(0, removeElementFromEndOfArray.length - 1)
-
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
 }
